b07a7839efbac5acf29c6153d81584d90e2e6951
Jacob Killelea <jkillelea@protonmail.ch>
2019-11-05 22:04:04 -0800

    decode temperature

--- src/devices/custom_thermistor.c
+++ src/devices/custom_thermistor.c
@@ -1,30 +1,46 @@
 #include "decoder.h"
+#include <math.h>
+
+// Thermistor consts
+static const double R1  = 18e3;
+static const double T25 = 273.15 + 25.0;
+static const double R25 = 10e3;
+static const double B25 = 3435.0;
+
 
 static int thermistor_decode_callback(r_device *decoder, bitbuffer_t *bitbuffer) {
-    data_t *data;
+    data_t  *data;
     uint8_t *b;
-    int id;
-    int len;
-    int payload;
+    int      id;
+    int      len;
+    int      payload;
 
-    // puts("");
     // bitbuffer_print(bitbuffer);
-    if (!((bitbuffer->bits_per_row[0] == 65) || (bitbuffer->bits_per_row[0] == 66)))
+
+    if (bitbuffer->bits_per_row[0] < 65)
+        return 0;
+    if (bitbuffer->bits_per_row[0] > 70)
         return 0;
 
     b = bitbuffer->bb[0];
 
-    id = (b[0] << 8*3) | (b[1] << 8*2) | (b[2] << 8*1) | (b[3]);
+    id  = (b[0] << 8*3) | (b[1] << 8*2) | (b[2] << 8*1) | (b[3]);
     len = b[4];
 
     // Sent LSBfirst
     payload = (b[5] << 8*0) | (b[6] << 8*1) | (b[7] << 8*2) | (b[8] << 8*3);
 
+    double vfrac = ((double) payload) / 1023.0;
+    double Rth   = -(vfrac*R1)/(vfrac-1);
+    double temperature = 1/(1/T25 + log(Rth/R25)/B25) - 273.15;
+
     data = data_make(
-            "model",  "",             DATA_STRING, _X("Custom Thermistor", "Custom Thermistor"),
-            "id",     "ID (16bit)",   DATA_FORMAT, "0x%x", DATA_INT, id,
-            "len",    "Len (8bit)",   DATA_FORMAT, "0x%x", DATA_INT, len,
-            "data",   "Data (32bit)", DATA_FORMAT, "%d",   DATA_INT, payload,
+            "model", "",   DATA_STRING, _X("Custom Thermistor", "Custom Thermistor"),
+            "id",   "ID (16bit)",       DATA_FORMAT, "0x%x", DATA_INT,    id,
+            "len",  "Len (8bit)",       DATA_FORMAT, "0x%x", DATA_INT,    len,
+            "data", "Raw Data (32bit)", DATA_FORMAT, "%d",   DATA_INT,    payload,
+            "data", "temperature_C",    DATA_FORMAT, "%f",   DATA_DOUBLE, temperature,
+            // "data",  "vfrac (32bit)", DATA_FORMAT, "%f",   DATA_DOUBLE, vfrac,
             NULL);
 
     decoder_output_data(decoder, data);
@@ -37,14 +53,15 @@ static char *output_fields[] = {
 };
 
 r_device custom_thermistor = {
-    .name          = "Custom Thermistor",
-    .modulation    = OOK_PULSE_PCM_RZ,
-    .short_width   = 1000, // 1000 us = 1 ms = 1kBaud
-    .long_width    = 1000,
-    .reset_limit   = 10000,
-    .sync_width    = 0,
-    .tolerance     = 100, // us
-    .decode_fn     = &thermistor_decode_callback,
-    .disabled      = 0,
-    .fields        = output_fields,
+    .name        = "Custom Thermistor",
+    // On Off Keying, Pulse Code Modulated, Return To Zero
+    .modulation  = OOK_PULSE_PCM_RZ,
+    .short_width = 1000, // 1000 us = 1 ms = 1kBaud
+    .long_width  = 1000,
+    .reset_limit = 10000,
+    .sync_width  = 0,
+    .tolerance   = 100, // us
+    .decode_fn   = &thermistor_decode_callback,
+    .disabled    = 0,
+    .fields      = output_fields,
 };
